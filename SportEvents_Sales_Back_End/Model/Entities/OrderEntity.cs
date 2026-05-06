63fa87fab1f3d8ad3f90dda74a65f19f137693e9
Aurelio <aurelio.iempereur@gmail.com>
2026-05-05 20:29:51 -0600

    nota

--- SportEvents_Sales_Back_End/Model/Entities/OrderEntity.cs
+++ SportEvents_Sales_Back_End/Model/Entities/OrderEntity.cs
@@ -9,7 +9,10 @@
         public DateTime DateEnd { get; set; }
         public int IdClient { get; set; }   
         public decimal SubTotal { get; set; }
-        public decimal TotalPrice { get; set; }
+        public decimal TotalPrice { get; set; }  
+
+
+
 
     }
 }
