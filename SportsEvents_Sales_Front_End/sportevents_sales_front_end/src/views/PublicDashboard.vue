367bcdc454f7ef60fe3702ed56c9bd6e0be986c4
Aurelio <aurelio.iempereur@gmail.com>
2026-05-06 22:24:11 -0600

    wiring the front-end

---
+++ SportsEvents_Sales_Front_End/sportevents_sales_front_end/src/views/PublicDashboard.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="container mt-5">
+        <div class="alert alert-info">
+            <h1>Public View</h1>
+            <p>This is accessible to everyone.</p>
+            <router-link to="/login-client" class="btn btn-primary">Go to Login</router-link>
+        </div>
+    </div>
+</template>
