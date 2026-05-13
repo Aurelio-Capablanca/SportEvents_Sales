76d8bfe3248e88d603c28fd2998c9af21746aad1
Aurelio <aurelio.iempereur@gmail.com>
2026-05-12 22:43:41 -0600

    Update View Elements
    + using Axios, for fetching

--- SportsEvents_Sales_Front_End/sportevents_sales_front_end/src/views/Public/EventsView.vue
+++ SportsEvents_Sales_Front_End/sportevents_sales_front_end/src/views/Public/EventsView.vue
@@ -1,60 +1,74 @@
 <script setup>
 import { ref } from 'vue'
+import { onMounted } from 'vue'
+import axios from 'axios'
 
-const events = ref([
-  {
-    id: 1,
-    title: 'Rock Concert',
-    quantity: 2,
-    date: "10/10/2040",
-    price: 40
-  },
-  {
-    id: 2,
-    title: 'Jazz Festival',
-    quantity: 1,
-    date: "10/10/2040",
-    price: 25
+
+const events = ref([]);
+const token = localStorage.getItem('token');
+
+
+onMounted(() => {
+  fetchEvents()
+});
+
+const fetchEvents = async () => {
+  console.log('Loading events...')
+  try {    
+    const response = await axios.get(
+      'http://192.168.122.44:5105/ticket-api/ticket-get-all',
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    )
+    console.log(response.data);
+    if (response.data.status == 200) {
+      events.value = response.data.dataset
+    }
+  } catch (error) {
+    console.error('View error:', error)
   }
-])
+  console.log('events : ',events.value)
+}
 </script>
 
 
 <template>
-    <div class="mb-4">
-      <h1 class="fw-bold">
-        Upcoming Games
-      </h1>
-      <p class="text-muted">
-        Browse available Soccer Games
-      </p>
-    </div>
-    <div class="row g-4">
-      <div class="col-md-6 col-lg-4" v-for="event in events" :key="event.id">
-        <div class="card h-100 shadow-sm">
-          <img :src="event.image" class="card-img-top" alt="">
-          <div class="card-body">
-            <h5 class="card-title">
-              {{ event.title }}
-            </h5>
-            <p class="text-muted">
-              {{ event.date }}
-            </p>
-            <p class="card-text">
-              {{ event.description }}
-            </p>
-          </div>
-          <div class="card-footer bg-white border-0">
-            <div class="d-flex justify-content-between align-items-center">
-              <span class="fw-bold">
-                ${{ event.price }}
-              </span>
-              <router-link :to="'/events/' + event.id" class="btn btn-primary">
-                View
-              </router-link>
-            </div>
+  <div class="mb-4">
+    <h1 class="fw-bold">
+      Upcoming Games
+    </h1>
+    <p class="text-muted">
+      Browse available Soccer Games
+    </p>
+  </div>
+  <div class="row g-4">
+    <div class="col-md-6 col-lg-4" v-for="event in events" :key="event.id">
+      <div class="card h-100 shadow-sm">        
+        <div class="card-body">
+          <h5 class="card-title">
+            {{ event.localTeam }} vs {{ event.visitorTeam}}
+          </h5>
+          <p class="text-muted">
+            {{ event.location }}
+          </p>
+          <p class="card-text">
+            {{ event.location }}
+          </p>
+        </div>
+        <div class="card-footer bg-white border-0">
+          <div class="d-flex justify-content-between align-items-center">
+            <span class="fw-bold">
+              ${{ event.solePrice }}
+            </span>
+            <router-link :to="'/events/' + event.id" class="btn btn-primary">
+              View
+            </router-link>
           </div>
         </div>
       </div>
-    </div>  
+    </div>
+  </div>
 </template>
